4fd7b9f9bacee81b0e4a07e0ca7a8b3f82e2a4d2
M Purnnaa Mannoj <120481326+GamingCentral@users.noreply.github.com>
2024-06-24 08:02:45 +0530

    Solution update twoSum

--- twoSum.py
+++ twoSum.py
@@ -1,1 +1,14 @@
 #given an array then find indexes of elements which add up to given sum
+
+class Solution:
+    def targetedSum(self, arr, targetSum):
+        for i in arr:
+            if arr.index(targetSum-i):
+                return [arr.index(i), arr.index(targetSum-i)]
+        return -1
+        
+arr=[int(x) for x in input().split(' ')]
+targetSum=int(input())
+s=Solution()
+l = s.targetedSum(arr, targetSum)
+print(l)
